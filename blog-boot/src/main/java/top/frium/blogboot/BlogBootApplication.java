548fd5e46a611a9b649e2dbd760e718df0fee8ac
fyyyyyyy <877385364@qq.com>
2025-02-07 17:02:09 +0800

    Initial commit

---
+++ blog-boot/src/main/java/top/frium/blogboot/BlogBootApplication.java
@@ -0,0 +1,13 @@
+package top.frium.blogboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogBootApplication.class, args);
+    }
+
+}
