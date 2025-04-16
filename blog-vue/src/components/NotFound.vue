102bf6cd6efa0274c6d43eeed3562081bd8d76e9
fyyyyyyy <877385364@qq.com>
2025-04-16 21:45:45 +0800

    feat(NotFound): 添加404组件

---
+++ blog-vue/src/components/NotFound.vue
@@ -0,0 +1,35 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="not-found">
+    <p class="text"> 404</p>
+    <p class="msg">
+      <span>找不到网页，可能已被删除，去</span>
+      <RouterLink :to="{ name: 'Home' }" style="color: #f8c0bf;">首页</RouterLink>
+      <span>看看吧!</span>
+    </p>
+
+  </div>
+</template>
+
+<style scoped lang="scss">
+.not-found {
+  background: rgba(40, 44, 52, 0.6);
+  height: 600px;
+  border-radius: 7px;
+
+  .text {
+    font-size: 77px;
+    font-style: italic;
+    margin: auto;
+    text-align: center;
+  }
+
+  .msg {
+    font-size: 20px;
+    text-align: center;
+  }
+}
+</style>
