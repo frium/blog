377e19173291a2071780a44ac9c0cfbd168ac97a
fyyyyyyy <877385364@qq.com>
2025-10-16 14:48:55 +0800

    perf: 修改字体逻辑为cdn

--- blog-vue/src/main.js
+++ blog-vue/src/main.js
@@ -4,22 +4,11 @@ import pinia from "./stores";
 import "@/styles/common.scss";
 import App from "./App.vue";
 import router from "./router";
+import "element-plus/dist/index.css";
+
 const app = createApp(App);
 
 app.use(pinia);
 app.use(router);
-
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/sw.js")
-      .then((registration) => {
-        console.log("Service Worker 注册成功: ", registration.scope);
-      })
-      .catch((error) => {
-        console.log("Service Worker 注册失败: ", error);
-      });
-  });
-}
 
 app.mount("#app");
